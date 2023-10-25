875f42d0f9542138f34625f3bcfae98ad6fd19dd
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-10-25 23:52:45 +0300

    Added Animations In Section Component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { FiChevronDown } from "react-icons/fi";
+import { Fade } from "react-reveal";
 
 const Section = ({
   title,
@@ -25,23 +26,29 @@ const Section = ({
         alignItems: "center",
       }}
     >
-      <div className="pt-[15vh] text-center">
-        <h1 className="text-4xl font-bold">{title}</h1>
-        <p className="text-sm p-2">{description}</p>
-      </div>
-      <div>
-        <div className="flex flex-col items-center justify-center text-sm lg:flex-row mb-[50px]">
-          <a href="#">
-            <button className="rounded-md bg-[#393c41] w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white">
-              {leftBtnText}
-            </button>
-          </a>
-          {rightBtnText && (
-            <button className="rounded-md bg-[#f4f4f4] w-96 lg:w-64 h-10 lg:mx-4 mt-2">
-              {rightBtnText}
-            </button>
-          )}
+      <Fade bottom>
+        <div className="pt-[15vh] text-center">
+          <h1 className="text-4xl font-bold">{title}</h1>
+          <p className="text-sm p-2">{description}</p>
         </div>
+      </Fade>
+
+      <div>
+        <Fade bottom>
+          <div className="flex flex-col items-center justify-center text-sm lg:flex-row mb-[50px]">
+            <a href="#">
+              <button className="rounded-md bg-[#393c41] w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white">
+                {leftBtnText}
+              </button>
+            </a>
+            {rightBtnText && (
+              <button className="rounded-md bg-[#f4f4f4] w-96 lg:w-64 h-10 lg:mx-4 mt-2">
+                {rightBtnText}
+              </button>
+            )}
+          </div>
+        </Fade>
+
         <div className="flex justify-center animate-bounce mt-[20px] h-[40px]">
           <FiChevronDown size={24} color="white" />
         </div>
