a4d9a7295b7bf7d668d158baf652dc34fcb6ba42
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-10-27 22:16:41 +0300

    Added login & logout Reducers In generalSlice In Redux Store

--- src/store/slices/generalSlice.js
+++ src/store/slices/generalSlice.js
@@ -7,7 +7,16 @@ const initialState = {
 export const generalSlice = createSlice({
   name: "generalSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
 });
+
+export const { login, logout } = generalSlice.actions;
 
 export default generalSlice.reducer;
