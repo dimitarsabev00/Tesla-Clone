fd9f280159ad56d6ddbf1d6411b75b0b81ad1424
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-11-04 13:29:32 +0200

    Change "Login" Route To "Sign In" Route & Added Sign Up Route In RoutesComp Component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
+import { useSelector } from "react-redux";
 import Login from "../screens/Login";
 import Home from "../screens/Home";
+import Register from "../screens/Register";
 
 const RoutesComp = () => {
+  const currentUser = useSelector(({ generalSlice }) => generalSlice.user);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/auth/sign-in"
+        element={currentUser ? <Navigate to="/account" /> : <Login />}
+      />
+      <Route
+        path="/auth/sign-up"
+        element={currentUser ? <Navigate to="/account" /> : <Register />}
+      />
     </Routes>
   );
 };
