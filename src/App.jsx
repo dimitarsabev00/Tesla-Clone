2dc88998e05aa03464fab5337b5d327dbf3eee56
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-11-07 20:26:19 +0200

    Added Persist CurrentUser Functionality In App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesComp from "./components/Routes";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/slices/generalSlice";
+import { auth } from "./configs/firebase";
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    onAuthStateChanged(auth,(userAuth) => {
+      if (userAuth) {
+        // User is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        )
+      } else {
+        // User is signed out
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
   return (
     <Router>
       <RoutesComp />
