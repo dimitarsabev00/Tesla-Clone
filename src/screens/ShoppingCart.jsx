d114a4a938c0aafd8da2f153f8efaa013ec19c86
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-12-07 18:14:36 +0200

    Added Styling For "Remove From Cart" Button In ShoppingCart Screen

--- src/screens/ShoppingCart.jsx
+++ src/screens/ShoppingCart.jsx
@@ -26,8 +26,11 @@ const ShoppingCart = () => {
           <li key={item.id} className="my-[20px]">
             <div>
               <div>{item.title}</div>
-              <div>{item.price}</div>
-              <button onClick={() => removeFromCart(item.id)}>
+              <div>{`$${item.price}`}</div>
+              <button
+                className="border border-solid border-black bg-transparent px-[8px] py-[8px] cursor-pointer text-black mt-[10px] transition-colors duration-300 hover:bg-black hover:text-white rounded-md"
+                onClick={() => removeFromCart(item.id)}
+              >
                 Remove From Cart
               </button>
             </div>
