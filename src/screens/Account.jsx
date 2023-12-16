183c97d355f78fa1e0de59716a9337199da0a8e2
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-12-16 19:12:49 +0200

    Fix Bug With Padding-Top In Account Screen Container Wrapper For Screen

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -8,7 +8,7 @@ const Account = () => {
   return (
     <div className="teslaAccount">
       <Header />
-      <div className="flex justify-between items-center py-[60px] px-[140px] pb-[30px]">
+      <div className="flex justify-between items-center pt-[100px] px-[140px] pb-[30px]">
         <div className="teslaAccount__person">
           <h4 className="font-medium text-xl capitalize">
             {currentUser?.displayName + "'s"} Account
